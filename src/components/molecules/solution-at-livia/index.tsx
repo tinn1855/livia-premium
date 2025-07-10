950a852254f07b457eb6d9e9e4a33231e70592eb
tinnguyen <tinn1855@gmail.com>
2025-07-10 11:34:06 +0700

    code UI solution at livia

---
+++ src/components/molecules/solution-at-livia/index.tsx
@@ -0,0 +1,44 @@
+export function SolutionAtLivia() {
+  return (
+    <section>
+      <div className="container mx-auto px-5 text-center py-5 space-y-5">
+        <h2 className="font-bold text-lg text-primary">
+          Giải pháp tại Livia Premium
+        </h2>
+        <p className="text-3xl">
+          TOÀN DIỆN PHÁC ĐỒ ĐIỀU TRỊ KẾT HỢP ĐA PHƯƠNG THỨC
+        </p>
+        <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-4 text-white">
+          <div className="col-span-1 bg-primary aspect-square items-center flex flex-col justify-center space-y-3 px-6 py-2">
+            <h3 className="text-8xl">01</h3>
+            <hr className="w-32" />
+            <span className="text-xl">Tư vấn thiết lập phác đồ điều trị</span>
+          </div>
+          <div className="col-span-1 bg-primary aspect-square items-center flex flex-col justify-center space-y-3 px-6 py-2">
+            <h3 className="text-8xl">02</h3>
+            <hr className="w-32" />
+            <span className="text-xl">chăm sóc chuẩn bị da bằng đường bôi</span>
+          </div>
+          <div className="col-span-1 bg-primary aspect-square items-center flex flex-col justify-center space-y-3 px-6 py-2">
+            <h3 className="text-8xl">03</h3>
+            <hr className="w-32" />
+            <span className="text-xl">
+              Thực hiện liệu trình điều trị tại clinic
+            </span>
+          </div>
+          <div className="col-span-1 bg-primary aspect-square items-center flex flex-col justify-center space-y-3 px-6 py-2">
+            <h3 className="text-8xl">04</h3>
+            <hr className="w-32" />
+            <span className="text-xl">
+              chăm sóc da sau điều trị bằng đường bôi
+            </span>
+          </div>
+        </div>
+        <p className="font-semibold lg:px-60">
+          ĐỘI NGŨ CHUYÊN VIÊN ĐỒNG HÀNH 1:1 CÙNG BẠN VỚI SỰ THEO DÕI CỦA BÁC SĨ
+          DA LIỄU TRONG SUỐT QUÁ TRÌNH ĐIỀU TRỊ
+        </p>
+      </div>
+    </section>
+  );
+}
--- src/components/pages/home/index.tsx
+++ src/components/pages/home/index.tsx
@@ -2,12 +2,14 @@ import { Footer } from '@/components/common/footer';
 import { Header } from '@/components/common/header';
 import { HeroSection } from '@/components/common/hero-section';
 import { FormConsultation } from '@/components/molecules/form-consultation';
+import { SolutionAtLivia } from '@/components/molecules/solution-at-livia';
 
 export function Home() {
   return (
     <>
       <Header />
       <HeroSection />
+      <SolutionAtLivia />
       <FormConsultation />
       <Footer />
     </>
